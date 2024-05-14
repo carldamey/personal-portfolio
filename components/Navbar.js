b1f6cce69dea5324c14787fc41d6853935bb6b53
carldamey <carldamey@gmail.com>
2024-05-14 16:34:30 -0700

    adds contact page and begins pdf download integration

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,14 @@
 import Link from "next/link"
+// import Resume from "../public/carl_damey_resume"
 
 export default function Navbar() {
 	return (
 		<nav>
 			<Link href="/">Home</Link>
-			<Link href="/about">DOWNLOAD MY RESUME</Link>
 			<Link href="/contact">CONTACT ME!</Link>
+			{/* <a href={Resume} download="Carl Damey Resume">DOWNLOAD MY RESUME */}
+        {/* <button className={classes.navLink}>DOWNLOAD MY RESUME</button> */}
+      {/* </a> */}
 		</nav>
 	)
 }
