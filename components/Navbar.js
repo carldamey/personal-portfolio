3e8f5c2ba68be4d9452ba2d829717ec8b4aaa46f
carldamey <carldamey@gmail.com>
2024-05-13 17:30:46 -0700

    modifies navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link"
 
 export default function Navbar() {
-  return(
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-    </nav>
-  )
+	return (
+		<nav>
+			<Link href="/">Home</Link>
+			<Link href="/about">DOWNLOAD MY RESUME</Link>
+			<Link href="/contact">CONTACT ME!</Link>
+		</nav>
+	)
 }
