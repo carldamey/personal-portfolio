284100edbf71bde8ef8fd9e021683182b7034c7b
carldamey <carldamey@gmail.com>
2024-05-15 15:59:12 -0700

    continues work on pdf support, reconfigures build for next.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,26 @@
 import Link from "next/link"
 // import Resume from "../public/carl_damey_resume"
 
+const RESUME_URL = "http://locahost:3000/public/carl_damey_resume.pdf"
+
+
 export default function Navbar() {
+  function downloadFile(url) {
+    const aTag = document.createElement("a")
+    aTag.href=url
+    aTag.setAttribute("download", "carl_damey_resume.pdf")
+    document.body.appendChild("aTag")
+    aTag.click()
+    aTag.remove()
+  }
+
 	return (
 		<nav>
 			<Link href="/">Home</Link>
+      <Link href="/about">ABOUT</Link>
 			<Link href="/contact">CONTACT ME!</Link>
+      <button onClick={downloadFile(RESUME_URL)}>DOWNLOAD MY RESUME</button>
+      <h1>test</h1>
 			{/* <a href={Resume} download="Carl Damey Resume">DOWNLOAD MY RESUME */}
         {/* <button className={classes.navLink}>DOWNLOAD MY RESUME</button> */}
       {/* </a> */}
