1f36ed3d80bd3c28aca09cbb6ba0fe6a9f72bb14
carldamey <carldamey@gmail.com>
2024-05-15 17:40:42 -0700

    finishes integration of resume download feature

---
+++ components/ResumeDownload.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function ResumeDownload() {
+	return (
+		<div>
+      <a href = "./carl_damey_resume.pdf" download = "Carl Damey Resume.pdf">DOWNLOAD MY RESUME</a>
+    </div>
+	)
+}
