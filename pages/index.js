9118f6011108d922629dd600c182f70434c2c2db
carldamey <carldamey@gmail.com>
2024-05-16 16:25:21 -0700

    fixes navbar orientation

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Image from "next/image"
 import {Inter} from "next/font/google"
 import styles from "@/styles/Home.module.css"
+import Navbar from "@/components/Navbar"
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -23,6 +24,7 @@ export default function Home() {
 					href="/favicon.ico"
 				/>
 			</Head>
+			<Navbar/>
 			<body className=" bg-red-500 flex items-center">
 				<h1 className="text-3xl font-bold underline">carl mode</h1>
 				<p>
